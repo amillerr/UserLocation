7536c9b93b1853e2e9356024b8fe9a1887dd1ce2
Tom Miller <62253468+amillerr@users.noreply.github.com>
2022-10-12 09:53:00 +0300

    Initial Commit

---
+++ UserLocation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UserLocation
+//
+//  Created by Artem Axenov on 2022-10-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserLocation/UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationApp.swift
+//  UserLocation
+//
+//  Created by Artem Axenov on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
